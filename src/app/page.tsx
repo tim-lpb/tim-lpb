c1daa433ab0d1a3c9be7af8aed706023c62f4a3e
tim-lpb <tbernards@arcor.de>
2026-03-31 16:58:14 +0200

    Style update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,25 @@ export default function Home() {
         <>
             <div className="flex flex-1 flex-col">
                 <div className="flex-1 overflow-y-auto">
-                    <div className="text-primary mx-auto flex max-w-2xl flex-1 flex-col gap-8 py-16 leading-[1.6] sm:py-32">
-                        <Image
-                            src="/img/avatar.jpeg"
-                            alt="Avatar of Tim"
-                            width={60}
-                            height={60}
-                            draggable={false}
-                            className="mb-8 rounded-full select-none"
-                        />
+                    <div className="text-primary mx-auto flex max-w-2xl flex-1 flex-col gap-16 py-16 leading-[1.6] sm:py-32">
+                        <section className="flex flex-col gap-4 px-4">
+                            <Image
+                                src="/img/avatar.jpeg"
+                                alt="Avatar of Tim"
+                                width={100}
+                                height={100}
+                                draggable={false}
+                                className="mb-8 rounded-full select-none"
+                            />
 
-                        <h1 className="text-2xl font-semibold">
-                            Tim
-                        </h1>
+                            <h1 className="text-2xl font-semibold">
+                                Tim
+                            </h1>
 
-                        <p className="text-secondary text-2xl font-semibold text-pretty">
-                            Trying to learn and build cool things. Currently working on this portfolio.
-                        </p>
+                            <p className="text-secondary text-2xl font-semibold text-pretty">
+                                Trying to learn and build cool things. Currently working on this portfolio.
+                            </p>
+                        </section>
                     </div>
                 </div>
             </div>
